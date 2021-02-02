5a9b65d6594fc28fd5d1e1546a57011386964b18
Léo Tomas <ltomas@enseirb-matmeca.fr>
2021-02-03 00:29:16 +0100

    project files

---
+++ wpbootstrap/index.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+
+	<link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
+	<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid-green">
+      <div class="row">
+        
+        <div class="col-1 col-lg-6">
+            <div>
+              <img src="<?php echo get_template_directory_uri(); ?>/img/motionDesign.png" />
+            </div>
+            <p class="smallText paragraphMiddle">
+              Technique qui prend sa place dans le marché de la communication, le motion design est là pour animer vos logos, vos habillages vidéo et vos illustrations les plus folles.
+              <br/>
+              De la publicité à la vidéo d'entreprise en passant par la présentation de produits ou de processus ainsi que la réalisation de formation, le motion design est présent dans les stratégies de communication des grandes entreprises comme des PME. Et la vôtre ?
+            </p>
+            <p class="followingParagraph">
+              <b>
+                Habillage vidéo ou motion design illustratif, nous trouvons l'agence qui répondra le mieux à vos besoins!
+              </b>
+            </p>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/firstPicture.png" class="img-bottom-firstPicture"/>
+        </div>
+
+        <div class="col-2 col-lg-1">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/imgCenterWhite.png" class="img-bottom"/>
+        </div>
+
+        <div class="col-3 col-lg-5">
+          <div class="text-center">
+            <b>
+              NOUS TROUVONS VOTRE FUTURE AGENCE DE
+              <br/>
+              MOTION DESIGN
+            </b>
+          </div>
+          
+          <div class="form">
+            <label for="name">Nom*</label>
+          </div>
+          <div>
+            <input name="name" class="inputName" placeholder="Tapez votre nom...">
+          </div>
+          <div class="input-group form">
+            <input name="email" class="half-input-left" placeholder="E-mail*">
+            <input name="tel" class="half-input-right" placeholder="Téléphone*">
+          </div>
+          <div class="form">
+            <input name="society" placeholder="Société">
+          </div>
+          <div class="form">
+          <input name="message" placeholder="Message*">
+          </div>
+
+          <div class="text-center buttonDiv">
+            <button class="buttonForm text-center">C'EST PARTI!</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+
+
+
+    <div class="container-fluid-uncolored">
+      <div class="row">
+        <a id="storyboard"></a>
+
+        <div class="col-4 col-lg-1"></div>
+
+        <div class="col-5 col-lg-4">
+          <div>
+            <span class="numberFont">
+              01
+            </span>
+            <span class="titleFont">
+              <b>DÉFINITION D'UN STORYBOARD</b>
+            </span>
+          </div>
+          <p class="smallText paragraphMiddle">
+            Le storyboard est l'âme même de votre vidéo. Sans un bon storyboard, l'équipe d'illustrateurs et motion designer ne pourront pas donner vie à votre vidéo.
+          </p>
+          <p class="smallText followingParagraph">
+            Pour créer le storyboard, il vous faut de grandes feuilles, un crayon, une gomme et de l'imagination. Réaliser cette étape avec votre agence vous permettra de savoir ce qu'il est possible ou non de faire en terme d'animation.
+          </p>
+          <p class="smallText followingParagraph">
+            Il est important que le storyboard soit validé avant tout démarrage de projet. Cette étape évitera à l'équipe créative de recommencer son travail à plusieurs reprises et de dépasser des délais souvent serrés. L'agence vous accompagnera dans ces différentes étapes de structuration pour que votre projet soit le plus efficace possible.
+          </p>
+          <div class="buttonDiv text-right">
+            <button class="otherButton text-center">Besoin d'une agence de motion design?</button>
+          </div>
+        </div>
+
+        <div class="col-2 col-lg-2">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/imgCenterGreen.png" class="img-up-center"/>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/imgCenterGreen.png" class="img-bottom-center"/>
+        </div>
+
+        <div class="col-2 col-lg-4 d-flex align-items-center">
+            <img class="halfScreenImg" src="<?php echo get_template_directory_uri(); ?>/img/secondPicture.png"/>
+        </div>
+
+        <div class="col-4 col-lg-1">
+          <div class="ancorTop">
+            1
+          </div>
+          <div class="ancorBottom">
+            <a href="#identity">2</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <div class="container-fluid-uncolored">
+      <div class="row">
+        <a id="identity"></a>
+        <div class="col-4 col-lg-1"></div>
+
+        <div class="col-2 col-lg-4 d-flex align-items-center">
+            <img class="halfScreenImg" src="<?php echo get_template_directory_uri(); ?>/img/thirdPicture.png"/>
+        </div>
+
+        <div class="col-2 col-lg-2">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/imgCenterGreen.png" class="img-up-center"/>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/imgCenterGreen.png" class="img-bottom-center"/>
+        </div>
+
+        <div class="col-5 col-lg-4">
+          <div>
+            <span class="titleFont">
+              <b>CRÉATION/REPRISE DE L'IDENTITÉ</b>
+            </span>
+            <span class="numberFont">
+              02
+            </span>
+          </div>
+
+          <p class="smallText paragraphMiddle">
+            <b>
+              Vous souhaitez une vidéo entièrement illustrative?
+            </b>
+            <br/>
+            Ce n'est pas un souci! Nos agences sont en mesure de créer tout un univers graphique autour de votre charte graphique. Elles pourront également vous proposer de créer des illustrations 100% personnalisées.
+          </p>
+          <p class="smallText followingParagraph">
+            <b>
+              Vous avez simplement besoin d'un habillage vidéo?
+            </b>
+            <br/>
+            Même si un habillage vidéo peut s'avérer simple graphiquement, nos agences ont conscience que cela doit correspondre à votre entreprise et respectent ainsi vos contraintes graphiques.
+          </p>
+          <p class="smallText followingParagraph"> 
+            Ainsi, pour une vidéo entièrement illustrative ou un habillage, nos agences font de votre charte graphique leur base de création.
+          </p>
+          <div class="buttonDiv text-right">
+            <button class="otherButton text-center">Chercher mon agence de motion design</button>
+          </div>
+        </div>
+
+        <div class="col-4 col-lg-1">
+          <div class="ancorTop">
+            <a href="#storyboard">1</a>
+          </div>
+          <div class="ancorBottom">
+            2
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+
+
+
+
+  </body>
+</html>
